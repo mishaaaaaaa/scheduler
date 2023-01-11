6f7f63f6bc3ea1ebb361af8b8f57ae33a3b9fb4d
mishaaaaaaa <misharudenkomisha@gmail.com>
2023-01-11 19:15:01 +0200

    fullfil table based on json logic

--- src/components/Scheduler.jsx
+++ src/components/Scheduler.jsx
@@ -16,20 +16,72 @@ import { headPeriod, bodyFakeData } from "../consts/fakeData";
 import TableHeadCell from "./TableHeadCell";
 import TableBodyCell from "./TableBodyCell";
 import { grey } from "@mui/material/colors";
+import days_json from "../consts/days.json";
 
-function createData(day, isSelected, data) {
-  return { day, isSelected, data };
+function createData(day, isSelectedRowPeriod, data) {
+  return { day, isSelectedRowPeriod, data };
+}
+
+function jsonToArray(json) {
+  let daysArr = [];
+
+  for (let i in json) {
+    daysArr.push({ day: i, period: json[i] });
+  }
+  return daysArr;
+}
+
+const daysFromJson = jsonToArray(days_json);
+
+function markCellOnPeriod(daysJson, rows) {
+  let res = [];
+  for (let i = 0; i < daysJson.length; i++) {
+    const daysPeriodJson = daysJson[i];
+    const daysPeriodRows = rows[i];
+    if (daysPeriodJson.period.length > 0) {
+      const startEndPeriodsIndex = daysPeriodJson.period.map((el) => {
+        let indexOfStartPeiod = daysPeriodRows.data.findIndex((cell) => {
+          return cell.cellMinutesPeriod.bt === el.bt;
+        });
+        let indexOfEndPeiod = daysPeriodRows.data.findIndex((cell) => {
+          return cell.cellMinutesPeriod.et === el.et;
+        });
+        return {
+          indexOfStartPeiod,
+          indexOfEndPeiod,
+        };
+      });
+
+      res.push({ periodPairs: startEndPeriodsIndex, weekIndex: i });
+    } else {
+      res.push({ peroidPairs: 0, dayIndex: i });
+    }
+  }
+  return res;
 }
 
 const rows = [
-  createData("MO", false, bodyFakeData),
-  createData("TU", false, bodyFakeData),
-  createData("WE", false, bodyFakeData),
-  createData("TH", false, bodyFakeData),
-  createData("FR", false, bodyFakeData),
-  createData("SA", false, bodyFakeData),
-  createData("SU", false, bodyFakeData),
+  createData("mo", false, bodyFakeData),
+  createData("tu", false, bodyFakeData),
+  createData("we", false, bodyFakeData),
+  createData("th", false, bodyFakeData),
+  createData("fr", false, bodyFakeData),
+  createData("sa", false, bodyFakeData),
+  createData("su", false, bodyFakeData),
 ];
+
+const test = markCellOnPeriod(daysFromJson, rows);
+
+function markCellWithPeriod(week, rows) {
+  const markedCells = week.map((day, dayIndex) => {
+    let dayPeriod;
+    const periodPairs = day.periodPairs;
+    if (periodPairs !== 0) {
+    }
+  });
+}
+
+console.log(test);
 
 export default function DenseTable() {
   const tableGray = grey[300];
--- src/consts/fakeData.js
+++ src/consts/fakeData.js
@@ -10,28 +10,172 @@ export const headPeriod = [
 ];
 
 export const bodyFakeData = [
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
+  {
+    cellMinutesPeriod: {
+      bt: 0,
+      et: 59,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 60,
+      et: 119,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 120,
+      et: 179,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 180,
+      et: 239,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 240,
+      et: 299,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 300,
+      et: 359,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 360,
+      et: 419,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 420,
+      et: 479,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 480,
+      et: 539,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 540,
+      et: 599,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 600,
+      et: 659,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 660,
+      et: 719,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 720,
+      et: 779,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 780,
+      et: 839,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 840,
+      et: 899,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 900,
+      et: 959,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 960,
+      et: 1019,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 1020,
+      et: 1079,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 1080,
+      et: 1139,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 1140,
+      et: 1199,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 1200,
+      et: 1259,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 1260,
+      et: 1319,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 1320,
+      et: 1379,
+    },
+    isSelectedCellPeriod: false,
+  },
+  {
+    cellMinutesPeriod: {
+      bt: 1380,
+      et: 1439,
+    },
+    isSelectedCellPeriod: false,
+  },
 ];
